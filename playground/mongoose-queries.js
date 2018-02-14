5459d2198074657ad2cd87f335e42fbf24e43448
erezstmn <erezstmn@gmail.com>
2018-02-14 12:40:21 +0200

    Add find by id route

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,30 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+// let id = '5a84073a09b0071419d16d5b';
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid')
+// }
+
+// Todo.find({
+//     _id:id
+// }).then((todos) =>{
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id:id
+// }).then((todo) =>{
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) =>{
+//     if (!todo){
+//         return console.log('ID not found');
+//     }
+//     console.log('Todo by ID', todo);
+// }).catch((e) => {
+//     console.log(e);
+// });
